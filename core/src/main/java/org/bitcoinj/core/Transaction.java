47a0b65130da1d621b2e85618457bb680f6c5e93
Danila Samosudov <samosudovd@gmail.com>
2018-02-22 16:21:25 +0300

    change ANYONECANPAY

--- core/src/main/java/org/bitcoinj/core/Transaction.java
+++ core/src/main/java/org/bitcoinj/core/Transaction.java
@@ -479,10 +479,10 @@ public class Transaction extends ChildMessage {
         ALL(1),
         NONE(2),
         SINGLE(3),
-        ANYONECANPAY(0x80), // Caution: Using this type in isolation is non-standard. Treated similar to ANYONECANPAY_ALL.
-        ANYONECANPAY_ALL(0x81),
-        ANYONECANPAY_NONE(0x82),
-        ANYONECANPAY_SINGLE(0x83),
+        ANYONECANPAY(128),
+        ANYONECANPAY_ALL(129),
+        ANYONECANPAY_NONE(130),
+        ANYONECANPAY_SINGLE(131),
         UNSET(0); // Caution: Using this type in isolation is non-standard. Treated similar to ALL.
 
         public final int value;
